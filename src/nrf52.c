0761dc43da223985777c3eb5550fef1a0d207d90
Kyunghwan Kwon <k@mononn.com>
2023-01-16 08:34:12 +0900

    Add device address type selection

--- src/nrf52.c
+++ src/nrf52.c
@@ -17,6 +17,7 @@
 #include <string.h>
 
 #include "ble.h"
+#include "ble_gap.h"
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
 #include "ble_advertising.h"
@@ -25,6 +26,7 @@ PBLE_STATIC_ASSERT(BLE_GAP_EVT_MAX < UINT8_MAX);
 
 #define BLE_PRIORITY			3
 #define ADV_INTERVAL_UNIT_THOUSANDTH	625
+#define DEFAULT_ADV_INTERVAL_MS		180
 
 enum ble_tag {
 	CONN_CFG_TAG			= 0,
@@ -60,20 +62,38 @@ static int adv_stop(struct ble *self);
 
 static void on_ble_events(ble_evt_t const * p_ble_evt, void * p_context)
 {
+	switch (p_ble_evt->header.evt_id) {
+	case BLE_GAP_EVT_CONNECTED:
+	case BLE_GAP_EVT_DISCONNECTED:
+	case BLE_GAP_EVT_PHY_UPDATE_REQUEST:
+	case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+	case BLE_GAP_EVT_AUTH_KEY_REQUEST:
+	case BLE_GAP_EVT_LESC_DHKEY_REQUEST:
+	case BLE_GAP_EVT_AUTH_STATUS:
+	case BLE_GATTC_EVT_TIMEOUT:
+	case BLE_GATTS_EVT_TIMEOUT:
+	default:
+		PBLE_LOG_INFO("EVT: %d", p_ble_evt->header.evt_id);
+		break;
+	}
 }
 
 static void on_error(uint32_t error_code)
 {
+	PBLE_LOG_ERROR("ERR: %x", error_code);
 }
 
 static void on_adv_event(ble_adv_evt_t ble_adv_evt)
 {
 	switch (ble_adv_evt) {
 	case BLE_ADV_EVT_FAST:
+		PBLE_LOG_INFO("Advertising started");
 		break;
 	case BLE_ADV_EVT_IDLE:
+		PBLE_LOG_INFO("Idle. sleep to save power");
 		break;
         default:
+		PBLE_LOG_INFO("ADV event: %d", ble_adv_evt);
 		break;
 	}
 }
@@ -134,19 +154,6 @@ static int initialize_advertising(struct ble *self,
 		handle->adv_handle = BLE_GAP_ADV_SET_HANDLE_NOT_SET;
 	}
 
-	/* advertising data */
-	handle->adv_data.adv_data.p_data = handle->enc_advdata[0];
-	memcpy(handle->adv_data.adv_data.p_data, self->adv.payload.payload,
-			self->adv.payload.index);
-	handle->adv_data.adv_data.len = self->adv.payload.index;
-
-	/* scan response data */
-	handle->adv_data.scan_rsp_data.p_data = handle->enc_scan_rsp_data[0];
-	memcpy(handle->adv_data.scan_rsp_data.p_data,
-			self->adv.scan_response.payload,
-			self->adv.scan_response.index);
-	handle->adv_data.scan_rsp_data.len = self->adv.scan_response.index;
-
 	handle->adv_params.primary_phy = BLE_GAP_PHY_1MBPS;
 	handle->adv_params.duration =
 		handle->adv_modes_config.ble_adv_fast_timeout;
@@ -175,13 +182,6 @@ static int initialize_advertising(struct ble *self,
 		break;
 	}
 
-	uint32_t err = sd_ble_gap_adv_set_configure(&handle->adv_handle, NULL,
-				&handle->adv_params);
-	if (err != NRF_SUCCESS) {
-		PBLE_LOG_ERROR("advertising configuration error: %x", err);
-		return -EINVAL;
-	}
-
 	handle->initialized = true;
 
 	return 0;
@@ -193,25 +193,27 @@ static int adv_start(struct ble *self)
 		return -ENODEV;
 	}
 
-	ble_adv_modes_config_t cfg = {
-		.ble_adv_fast_enabled = true,
-		.ble_adv_fast_interval = self->adv.min_ms * 1000UL /
-			ADV_INTERVAL_UNIT_THOUSANDTH,
-		.ble_adv_fast_timeout = self->adv.duration_ms / 10,
-	};
- 
-	if (initialize_advertising(self, &cfg)) {
-		PBLE_LOG_ERROR("advertising init failure");
-		return -EINVAL;
-	}
+	/* advertising data */
+	self->adv.handle.adv_data.adv_data.p_data =
+			self->adv.handle.enc_advdata[0];
+	memcpy(self->adv.handle.adv_data.adv_data.p_data,
+			self->adv.payload.payload, self->adv.payload.index);
+	self->adv.handle.adv_data.adv_data.len = self->adv.payload.index;
 
-	ble_advertising_conn_cfg_tag_set(&self->adv.handle, CONN_TAG);
+	/* scan response data */
+	self->adv.handle.adv_data.scan_rsp_data.p_data =
+			self->adv.handle.enc_scan_rsp_data[0];
+	memcpy(self->adv.handle.adv_data.scan_rsp_data.p_data,
+			self->adv.scan_response.payload,
+			self->adv.scan_response.index);
+	self->adv.handle.adv_data.scan_rsp_data.len =
+			self->adv.scan_response.index;
 
 	uint32_t err = ble_advertising_start(&self->adv.handle,
 			BLE_ADV_MODE_FAST);
 	if (err != NRF_SUCCESS) {
 		PBLE_LOG_ERROR("advertising start failure: %x", err);
-		return -EFAULT;
+		return -ENETDOWN;
 	}
 
 	return 0;
@@ -219,6 +221,7 @@ static int adv_start(struct ble *self)
 
 static int adv_stop(struct ble *self)
 {
+	sd_ble_gap_adv_stop(self->adv.handle.adv_handle);
 	return 0;
 }
 
@@ -228,37 +231,51 @@ static int adv_init(struct ble *self, enum ble_adv_mode mode)
 
 	self->adv.mode = mode;
 
-	self->adv.min_ms = 180;
-	self->adv.max_ms = 180;
-	self->adv.duration_ms = 180000;
+	self->adv.min_ms = DEFAULT_ADV_INTERVAL_MS;
+	self->adv.max_ms = DEFAULT_ADV_INTERVAL_MS;
+	self->adv.duration_ms = 0; /* forever */
+
+	ble_adv_modes_config_t cfg = {
+		.ble_adv_fast_enabled = true,
+		.ble_adv_fast_interval = self->adv.min_ms * 1000UL /
+			ADV_INTERVAL_UNIT_THOUSANDTH,
+		.ble_adv_fast_timeout = self->adv.duration_ms / 10,
+		.ble_adv_on_disconnect_disabled = true,
+	};
+
+	if (initialize_advertising(self, &cfg)) {
+		PBLE_LOG_ERROR("advertising init failure");
+		return -EINVAL;
+	}
+
+	ble_advertising_conn_cfg_tag_set(&self->adv.handle, CONN_TAG);
 
 	return 0;
 }
 
-static enum ble_device_addr get_device_address(struct ble *iface,
+static enum ble_device_addr get_device_address(struct ble *self,
 		uint8_t addr[BLE_ADDR_LEN])
 {
-	memcpy(addr, iface->addr, BLE_ADDR_LEN);
-	return iface->addr_type;
+	memcpy(addr, self->addr, BLE_ADDR_LEN);
+	return self->addr_type;
 }
 
-static int initialize(struct ble *iface)
+static int initialize(struct ble *self)
 {
-	(void)iface;
 	uint32_t ram_start = 0;
 
 	if (nrf_sdh_enable_request() != NRF_SUCCESS) {
 		PBLE_LOG_ERROR("softdevice not ready");
-		return -1;
+		return -ENOLINK;
 	}
 	if (nrf_sdh_ble_default_cfg_set(CONN_TAG, &ram_start) != NRF_SUCCESS) {
 		PBLE_LOG_ERROR("invalid softdevice configuration");
-		return -2;
+		return -EINVAL;
 	}
 	if (nrf_sdh_ble_enable(&ram_start) != NRF_SUCCESS) {
 		PBLE_LOG_ERROR("BLE start failure. Check RAM start address: %x",
 				ram_start);
-		return -3;
+		return -EADDRNOTAVAIL;
 	}
 
 	NRF_SDH_BLE_OBSERVER(event_handle, BLE_PRIORITY, on_ble_events, NULL);
@@ -269,6 +286,36 @@ static int initialize(struct ble *iface)
 			(const uint8_t *)BLE_DEFAULT_DEVICE_NAME,
 			sizeof(BLE_DEFAULT_DEVICE_NAME)-1) != NRF_SUCCESS) {
 		PBLE_LOG_ERROR("can not set device name");
+		return -EAGAIN;
+	}
+
+	ble_gap_privacy_params_t privacy_params = {
+		.privacy_mode = BLE_GAP_PRIVACY_MODE_DEVICE_PRIVACY,
+		.private_addr_type = BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE,
+		.private_addr_cycle_s =
+			BLE_GAP_DEFAULT_PRIVATE_ADDR_CYCLE_INTERVAL_S,
+		.p_device_irk = NULL,
+	};
+
+	switch (self->addr_type) {
+	case BLE_ADDR_PUBLIC:
+		privacy_params.private_addr_type = BLE_GAP_ADDR_TYPE_PUBLIC;
+		break;
+	case BLE_ADDR_STATIC_RPA:
+		privacy_params.private_addr_type =
+			BLE_GAP_ADDR_TYPE_RANDOM_STATIC;
+		break;
+	case BLE_ADDR_PRIVATE_NRPA:
+		privacy_params.private_addr_type =
+			BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_NON_RESOLVABLE;
+		break;
+	default: /* fall through */
+	case BLE_ADDR_PRIVATE_RPA:
+		break;
+	}
+
+	if (sd_ble_gap_privacy_set(&privacy_params) != NRF_SUCCESS) {
+		return -EAGAIN;
 	}
 
 	return 0;
@@ -277,17 +324,20 @@ static int initialize(struct ble *iface)
 static int enable_device(struct ble *self,
 		enum ble_device_addr addr_type, uint8_t addr[BLE_ADDR_LEN])
 {
+	int rc = 0;
+
 	if (!onair) {
 		self->addr_type = addr_type;
 		if (addr) {
 			memcpy(self->addr, addr, sizeof(self->addr));
 		}
 
-		initialize(self);
-		onair = self;
+		if ((rc = initialize(self)) == 0) {
+			onair = self;
+		}
 	}
 
-	return 0;
+	return rc;
 }
 
 static int disable_device(struct ble *self)
